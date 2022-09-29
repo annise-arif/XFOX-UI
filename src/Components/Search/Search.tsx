1ac48a00d6b1d1d71e77d404c301b701922c7abb
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-09-29 19:16:02 +0600

    fixing some console error

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import Form from "react-bootstrap/Form";
 import { CountriesModal } from "./CountriesModal/CountriesModal";
 import "./Search.css";
@@ -9,8 +9,10 @@ export const Search: FC = () => {
   return (
     <div className="Search pt-5">
       <div className="location">
-        <button onClick={() => setModalShow(true)}
-         className="locationBtn mx-auto mb-3">
+        <button
+          onClick={() => setModalShow(true)}
+          className="locationBtn mx-auto mb-3"
+        >
           <span className="locationIcon">
             <div className="icon--3D09z LocationIcon small--2q8vN gtm-search-click">
               <svg
@@ -24,13 +26,10 @@ export const Search: FC = () => {
           </span>
           <span>{Country}</span>
         </button>
-        <CountriesModal 
-             show={modalShow}
-             onHide={() => setModalShow(false)}
-             />
+        <CountriesModal show={modalShow} onHide={() => setModalShow(false)} />
       </div>
       <Form className="d-flex SearchForm position-relative">
-      <Form.Control
+        <Form.Control
           type="search"
           placeholder="What are you looking for?"
           className="SearchInput mx-auto rounded-5 px-4 py-3"
@@ -45,7 +44,7 @@ export const Search: FC = () => {
           >
             <path
               d="M7.615 15.23a7.615 7.615 0 1 1 6.1-3.054l2.966 2.967a1.088 1.088 0 0 1-1.539 1.538l-2.966-2.966a7.582 7.582 0 0 1-4.56 1.516zm5.44-7.615a5.44 5.44 0 1 1-10.88 0 5.44 5.44 0 0 1 10.88 0z"
-              fill-rule="evenodd"
+              fillRule="evenodd"
             ></path>
           </svg>
         </div>
--- src/Share/Header/Header.tsx
+++ src/Share/Header/Header.tsx
@@ -14,9 +14,8 @@ export const Header:FC = () => {
       <Navbar className="HeaderBg" expand="lg">
       <Container>
         <Navbar.Brand href="#">
-          <a href="#" className='text-white text-decoration-none fs-3 fw-bold'>
-          <h3>Xfox.com</h3>
-          </a>
+          <h3 className='text-white fs-3 fw-bold'>Xfox.com</h3>
+          
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
@@ -26,22 +25,21 @@ export const Header:FC = () => {
             navbarScroll
           >
             <Nav.Link href="#" className="text-white fw-semibold mx-2">All ads</Nav.Link>
-            <Nav.Link href="#">
-            <a href="#" className='text-white text-decoration-none'><small className=' border border-dark px-2 py-1 rounded-1'>বাংলা</small></a>
+            <Nav.Link href="#"><small className='text-white text-decoration-none border border-dark px-2 py-1 rounded-1'>বাংলা</small>
             </Nav.Link>
           </Nav>
 
-          <Nav.Link href="#"><a href="#" className='Chat text-decoration-none'>
+          <Nav.Link href="#"><span className='Chat text-decoration-none'>
           <div className="icon--3D09z medium--6L0i0" ><svg fill='white' viewBox="0 0 60 60" className="svg-wrapper--8ky9e"><path d="M38.05 30.85c-9.16.64-11.38-4.9-11.38-9.44a10.16 10.16 0 0 1 .26-2.25H20a10.57 10.57 0 0 0-2.14 20.77v4.5a1.6 1.6 0 0 0 1.57 1.6 1.53 1.53 0 0 0 1.02-.4l5.92-5.4H30a11.03 11.03 0 0 0 9.8-7.4z"></path><path d="M28.9 21.4a7.32 7.32 0 0 1 7.03-7.4h7.03a7.43 7.43 0 0 1 1.06 14.7v3.07a1.12 1.12 0 0 1-1.1 1.13 1.08 1.08 0 0 1-.72-.28l-.04-.04-.03-.03-3.72-3.73h-2.46a7.32 7.32 0 0 1-7.03-7.4"></path></svg></div>
             <small className="text-white me-5"> Chat</small>
-            </a>
+            </span>
           </Nav.Link>
             <Nav.Link href="#">
-            <a onClick={() => setModalShow(true)} href="#" className='Login text-white text-decoration-none'>
+            <span onClick={() => setModalShow(true)} className='Login cursore-pointer text-white text-decoration-none'>
             <div className="icon--3D09z medium--6L0i0"><svg fill='white' viewBox="0 0 60 60" className="svg-wrapper--8ky9e"><path d="M36.723 34.65c1.637 1.243 6.495 2.38 10.397 4.484 1.286.694 1.396 1.724 1.492 2.345.097.622.3 7.404.3 7.404H11s.204-6.782.3-7.404c.097-.621.105-1.565 1.493-2.345 3.866-2.169 8.738-3.169 10.375-4.412.65-.494.482-1.292.627-2.058.144-.766.626-.24.626-1.052 0-.819.153-.676.063-1.763-.078-.948-1.386-1.049-1.46-2.821-.015-.374-.674-.623-1.06-1.197-.385-.574-1.01-1.579-1.01-2.727s.24-.862.24-2.297-.053-2.267.771-5.598c.308-1.243 1.354-2.7 2.402-3.359 1.413-.888.845.296 5.593-.756 3.077-.682 7.898 2.488 7.946 5.024.065 3.43.276 3.172.48 4.593.145 1.005.434.766.434 1.914 0 1.15-.626 2.584-1.011 3.158-.385.575-.742 1.496-.792 1.866-.252 1.917-1.54 1.474-1.593 2.501-.05.938-.111.818.03 1.549.118.61.476-.04.62.726.146.765.045 1.89.649 2.225"></path></svg>
             </div>
               <small className='me-5'>Login</small>
-            </a>
+            </span>
             <LoginModal 
              show={modalShow}
              onHide={() => setModalShow(false)}
